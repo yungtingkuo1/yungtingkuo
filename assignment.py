78cafe062c797fef39c9f7f3dd15e02e59dad1f9
Kuo <yungting@amazon.com>
2023-06-09 20:47:38 +0100

    assignment wishes

---
+++ assignment.py
@@ -0,0 +1,17 @@
+print("What's your name?")
+name = input ("Provide your first name:")
+print("What's your age?")
+age = int (input("Provide the year of your birth:"))
+
+age_sum = 2023 - age
+
+print(f"\nHello {name},\n")
+
+print(f"let's celebrate your {age_sum} years of awesomeness!")
+print(f"Wishing you a day filled with joy and laughter as you turn {age_sum}!")
+
+print(f"May your wishes and dreams come true. Have a blessed year!")
+print(f"Now enjoy this speical cake has baked just for you!!\n")
+
+print(f"With love and best wishes,")
+print(f"YungTing")
