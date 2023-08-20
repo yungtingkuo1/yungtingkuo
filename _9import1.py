e0892477561023c02f5be2677373f4a6ad245650
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-08-20 14:11:59 +0100

    Add files via upload

--- _9import1.py
+++ _9import1.py
@@ -8,15 +8,7 @@ def load_file(file_name, default_return):
         print("File does not exist!Creating new file...")
         return default_return
 
-def load_account(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            return literal_eval(file.read())
-    except FileNotFoundError:
-        print("File does not exist!Creating new file...")
-        return 0
 
-
-def save_inventory(file_name, inventory):
+def save_file(file_name, value):
     with open(file_name, 'w') as file:
-        file.write(str(inventory))
+        file.write(str(value))
