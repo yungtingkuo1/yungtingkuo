2a54a5ebbd2c91afc36319cd1ad7fa4f0897c385
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-08-26 12:37:21 +0100

    Add files via upload

---
+++ _13pip.py
@@ -0,0 +1,88 @@
+#API_URL= https://api.open-meteo.com/v1/forecast?daily=precipitation_sum&end_date=%7Bsearched_date%7D%60&latitude=%7Blatitude%7D&longitude=%7Blongitude%7D&start_date=%7Bsearched_date%7D&timezone=Europe%2FLondon
+
+"""
+- weather_data_folder
+    \ 2023-08-26.pickle
+"""
+
+import requests
+import datetime
+import os
+import pickle
+
+expected_date_format = "YYYY-MM-DD"
+
+API_URL = "https://api.open-meteo.com/v1/forecast"
+Data_folder_path = "weather_data_folder"
+
+# check if data folder exists, if not create it
+if not os.path.exists(Data_folder_path):
+    os.makedirs(Data_folder_path)
+
+params = {
+    "daily":"precipitation_sum",
+    "end_date":"%7Bsearched_date%7D%60",
+    "latitude":"%7Blatitude%7D",
+    "longitude":"%7Blongitude%7D",
+    "start_date":"%7Bsearched_date%7D",
+    "timezone":"Europe%2FLondon",
+    "date":"2023-08-26",
+}
+
+#Getting date from the user
+expected_date_format = "YYYY-MM-DD"
+requested_date = input(f"Pass date you want to check in format ({expected_date_format}):")
+
+if not requested_date:
+    print("using tomorrow as date...")
+    requested_date = (datetime.datetime.today().date() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+else:
+    print("using user provided date...")
+
+# Validate date format
+# 1. doas it have 2 hyphens
+if not requested_date.count("-") == 2:
+    print(f"Wrong date format:{requested_date}, expected:{expected_date_format}")
+    quit()
+if not len([ch for ch in requested_date if ch.isdigit()]) == 8:
+    print(f"Wrong date format:{requested_date}, expected:{expected_date_format}")
+    quit()   
+year_part, month_part, day_part = requested_date.split('-')
+
+print(f"Date passed correctly: {requested_date}. Getting the data...")
+
+for daily in params["result"]:
+    if daily ["precipitation_sum"] > 0:
+        print("It will rain")
+    elif daily ["precipitation_sum"] == 0:
+        print("It will not rain")
+    else:
+        print("I don't know")
+
+# check if the date was alreday downloaded if yes, return value from file
+file_path = os.path.join(Data_folder_path, f"{requested_date}.pickle")
+
+if os.path.exists(file_path):
+    print("Date present in history, getting from cache...")
+    # open file, print the data
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+else:
+    print("Date not present in history, getting from API...")
+    # get the data from API, save to file
+    params["date"] = requested_date
+
+    #Getting requst
+    resp = requests.get(API_URL, params=params)
+
+    if resp.ok:
+        data = resp.json() 
+        with open(file_path, "wb") as f:
+            pickle.dump(data,f)
+    else:
+        date = {}
+        print(f"Request was not successful: {resp.text}")
+
+# here data should be dictionary
+
+print(data)
