1042d211fdb98842635ecb7b233a8d496c49e636
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-09-04 11:41:39 +0100

    Add files via upload

--- _15interface.py
+++ _15interface.py
@@ -1,5 +1,14 @@
 
 import requests
+import pickle
+import os
+
+percip = []
+
+with open("_13pip.py") as f:
+    for percip in f:
+         if "percip" in percip:
+            percip.append(percip)
 
 class WeatherForecast:
     def __init__(self, date, weather):
@@ -16,20 +25,59 @@ class WeatherForecast:
     def __iter__(self):
         for weather in self.forecast:
             yield weather
+        
+        for percip in f:
+            print(percip)
     
     def items(self):
-        for date, weather in wf.items():
+        for date, weather in percip.items():
             print({date}, {weather})
-
-wf = WeatherForecast()
+    
+API_URL = "https://api.open-meteo.com/v1/forecast"
+data_folder_path = "weather_data_folder"
+params = {
+    # "end_date":"%7Bsearched_date%7D%60",
+     # "start_date":"%7Bsearched_date%7D",
+    "daily":"precipitation_sum",  
+    "latitude":"51.50853",
+    "longitude":"-0.12574",  
+    "timezone":"Europe/London",
+}
+expected_date_format = "YYYY-MM-DD"
+requested_date = input(f"Pass date you want to check in format ({expected_date_format}):")
+file_path = os.path.join(data_folder_path, f"{requested_date}.pickle")
 
 def create_forecast():
-        resp = requests.get("https://api.open-meteo.com/v1/forecast")
-        if not resp.ok:
-            return None
-        resp = resp.json()["results"][0]
-        forcast = forcast(
-        forcast = resp["precipitation_sum"]
-        date = resp["date"],)
-        return True, forcast
+    if os.path.exists(file_path):
+        print("Date present in history, getting from cache...")
+    # open file, print the data
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+    else:
+        print("Date not present in history, getting from API...")
+    # get the data from API, save to file
+        params["start_date"] = requested_date
+        params["end_date"] = requested_date
+
+    #Getting requst
+    resp = requests.get(API_URL, params=params)
+
+    if resp.ok:
+        data = resp.json() 
+        with open(file_path, "wb") as f:
+            pickle.dump(data,f)
+    else:
+        data = {}
+        print(f"Request was not successful: {resp.text}")
+
+    precip = print(data.get("daily",{}).get("precipitation_sum",[-1])[0])
+
+    if precip > 0:
+        print("It will rain")
+    elif precip == 0:
+        print("It will not rain")
+    else:
+        print("I don't know")
+
+
     
