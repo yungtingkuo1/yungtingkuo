c60e248f31658a6fd75ff3cf17c1289b94695912
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-09-06 15:59:38 +0100

    Add files via upload

--- _15interface.py
+++ _15interface.py
@@ -3,12 +3,9 @@ import requests
 import pickle
 import os
 
-percip = []
-
-with open("_13pip.py") as f:
-    for percip in f:
-         if "percip" in percip:
-            percip.append(percip)
+API_URL = "https://api.open-meteo.com/v1/forecast"
+folder = "weather_data_folder"
+file_path = os.path.join(folder)
 
 class WeatherForecast:
     def __init__(self, date, weather):
@@ -16,68 +13,56 @@ class WeatherForecast:
         self.weather = weather
         self.forecast = {}
     
+    def get_from_api(self):
+        params = {
+            "daily":"precipitation_sum",  
+            "latitude":"51.50853",
+            "longitude":"-0.12574",  
+            "timezone":"Europe/London",
+        }
+
+        resp = requests.get(API_URL, params=params)
+
+        if resp.ok:
+            data = resp.json() 
+            with open(file_path, "wb") as f:
+                pickle.dump(data,f)
+        else:
+            data = {}
+            print(f"Request was not successful: {resp.text}")
+  
     def __setitem__(self, date, weather):
         self.forecast[date] = weather
 
     def __getitem__(self, date):
-        return self.forecast[date]
+        if os.path.exists(file_path):
+            with open(file_path, "rb") as f:
+                return pickle.load(f)
+        else:
+            return self.get_from_api[date]
     
     def __iter__(self):
-        for weather in self.forecast:
-            yield weather
-        
-        for percip in f:
-            print(percip)
-    
+        folder = "weather_data_folder"
+        for file_name in os.listdir(folder):
+            yield file_name.split('-')[0]
+            
     def items(self):
-        for date, weather in percip.items():
-            print({date}, {weather})
-    
-API_URL = "https://api.open-meteo.com/v1/forecast"
-data_folder_path = "weather_data_folder"
-params = {
-    # "end_date":"%7Bsearched_date%7D%60",
-     # "start_date":"%7Bsearched_date%7D",
-    "daily":"precipitation_sum",  
-    "latitude":"51.50853",
-    "longitude":"-0.12574",  
-    "timezone":"Europe/London",
-}
-expected_date_format = "YYYY-MM-DD"
-requested_date = input(f"Pass date you want to check in format ({expected_date_format}):")
-file_path = os.path.join(data_folder_path, f"{requested_date}.pickle")
+        folder = "weather_data_folder"
+        file_path = os.path.join(folder)
+        precip = data.get("daily",{}).get("precipitation_sum",[-1])[0]
 
-def create_forecast():
-    if os.path.exists(file_path):
-        print("Date present in history, getting from cache...")
-    # open file, print the data
-        with open(file_path, "rb") as f:
-            data = pickle.load(f)
-    else:
-        print("Date not present in history, getting from API...")
-    # get the data from API, save to file
-        params["start_date"] = requested_date
-        params["end_date"] = requested_date
-
-    #Getting requst
-    resp = requests.get(API_URL, params=params)
-
-    if resp.ok:
-        data = resp.json() 
-        with open(file_path, "wb") as f:
-            pickle.dump(data,f)
-    else:
-        data = {}
-        print(f"Request was not successful: {resp.text}")
-
-    precip = print(data.get("daily",{}).get("precipitation_sum",[-1])[0])
-
-    if precip > 0:
-        print("It will rain")
-    elif precip == 0:
-        print("It will not rain")
-    else:
-        print("I don't know")
-
-
-    
+        for file_name in os.listdir(folder):
+            if os.path.exists(file_path):
+                print("Date present in history, getting from cache...")
+            with open(file_path, "rb") as f:
+                data = pickle.load(f)
+        else:
+            yield (file_name.split('-')[0], precip)
+        
+        if precip > 0:
+            print("It will rain")
+        elif precip == 0:
+            print("It will not rain")
+        else:
+            print("I don't know")
+         
