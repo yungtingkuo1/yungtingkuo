e27404bf810fd14cabf699bf12df05eafee5f5b1
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-09-17 15:43:34 +0100

    Add files via upload

--- _20decorators.py
+++ _20decorators.py
@@ -1,78 +1,94 @@
+import _9import1
+
 class Manager:
     def __init__(self):
         self.actions = {}
     
+    def load_data(self):
+        self.file_name = input("Please provide a name of a file(include extension, i.e. .txt)")
+        self.file_name_account = input("Please provide a name of a file(include extension, i.e. .txt)")
+        self.file_name_history = input("Please provide a name of a file(include extension, i.e. .txt)")
+
+        self.inventory = _9import1.load_file(self.file_name, {})
+        self.account = _9import1.load_file(self.file_name_account, 0)
+        self.history = _9import1.load_file(self.file_name_history, [])
+    
+    def save_data(self):
+        _9import1.save_file(self.file_name, self.inventory)
+        _9import1.save_file(self.file_name_account, self.account)
+        _9import1.save_file(self.file_name_history, self.history)
+
+  
     def assign(self, name):
         def inner_function(func):
-            self.function[name] = func
+            self.actions[name] = func
 
         return inner_function
         
     def execute(self, name, *args, **kwargs):
-        return self.function[name](self,*args, *kwargs)
+        return self.actions[name](self,*args, *kwargs)
             
 manager = Manager ()
-sale = Sale()
+manager.load_data()
+
 
 @manager.assign("accounting")
-def accounting():
+def accounting(manager):
     print("Welcome to our accounting system!")
 
-@sale.assgin("sale")
-class Sale:
-    def sale():
-        product_name = input("Enter the product name: ")
-        price = float(input("Enter the product price: "))
-        quantity = float(input("Enter the product quantity: "))
+@manager.assign("sale")
+def sale(manager):
+    product_name = input("Enter the product name: ")
+    price = float(input("Enter the product price: "))
+    quantity = float(input("Enter the product quantity: "))
+    if manager.inventory.get(product_name, 0) < quantity:
+        print(f"Not enought itmes: {product_name}")
+        return
+            
+    manager.history.append([product_name,price, quantity]) 
+    manager.inventory[product_name] -= quantity
+    manager.account += quantity * price
 
-        manager.execute(product_name, price, quantity)
-        print(f"Sale: {product_name, price, quantity}")
+@manager.assign("purchase")
+def purchase(manager):
+    product_name = input("Enter the product name: ")
+    price = float(input("Enter the product price: "))
+    quantity = float(input("Enter the product quantity: "))
+        
+    if manager.account >= quantity * price:
+        manager.history.append([product_name,price, quantity]) 
+        manager.inventory[product_name] -= quantity
+        manager.account += quantity * price
+    else:
+        print(f"Insufficient funds. Purchase cannot be completed.")
 
-@purchase.assgin("purchase")
-def purchase(func):
-    def wrapper(*args, **kwargs):
-        # Add purchase functionality here
-        # ...
-        return func(*args, **kwargs)
-    return wrapper
+@manager.assign("balance")
+def balance(manager):
+    amount = float(input("Enter the amount to add (+) or subtract (-): "))
+    if manager.amount + manager.account >= 0:
+        manager.account += amount
+    else:
+        print("Error! account can not be negative!")
 
-@balance.assgin("balance")
-def balance(func):
-    def wrapper(*args, **kwargs):
-        # Add balance functionality here
-        # ...
-        return func(*args, **kwargs)
-    return wrapper
+@manager.assign("review")
+def review(manager):
+    review = []
+    print("Provide from and to values")
+    from_value = int(input("From:"))
+    to_value = int(input("To:"))
+    new_review = manager.history[from_value:to_value]
+    print(new_review)
 
 
 while True:
     action = input("What action to do [sale, purchase or balance]")
     if action == "sale":
-        product_name = input("Enter the product name: ")
-        price = float(input("Enter the product price: "))
-        quantity = float(input("Enter the product quantity: "))
-
-        manager.execute(product_name, price, quantity)
-        print(f"Sale: {product_name, price, quantity}")
-
-    elif action == "purchase":
-        product_name = input("Enter the product name: ")
-        price = float(input("Enter the product price: "))
-        quantity = float(input("Enter the product quantity: "))
-            
-        manager.execute(product_name, price, quantity)
-        print(f"Purchase: {product_name, price, quantity}")
-
+        manager.execute("sale")
+    elif action == "purchase":        
+        manager.execute("purchase")
     elif action == "balance":
-        amount = float(input("Enter the amount to add (+) or subtract (-): "))
-        
-        manager.execute(amount)
-        print(f"balance: {amount}")
-    
+        manager.execute("balance")
     else:
         print("Exiting...")
+        manager.save_data()
         break
-
-
-
-
