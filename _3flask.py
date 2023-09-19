9d77e42931b7428186bb361825ae1d5cb3a5f6c1
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-09-19 19:07:58 +0100

    Add files via upload

--- _3flask.py
+++ _3flask.py
@@ -2,11 +2,19 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
-from database import load_database
-from database import save_database
+import json
 
 
 app= Flask(__name__)
+
+def load_data():
+    with open ("product.json","r") as f:
+        data = json.load(f)
+    return data
+
+def save_data(data):
+    with open ("product.json","r") as f:
+        json.dump(data.f)
 
 # http://127.0.0.1:5000/
 @app.route("/")
@@ -16,7 +24,25 @@ def main_page():
 #http://127.0.0.1:5000/purchaseform
 @app.route("/purchaseform")
 def purchase():
-    return render_template("purchaseform.html")
+    if request.method == "POST":  
+    # load data from file
+    data = load_data()
+    # get data from form
+    purchase_data = data.get(purchase)
+    # request.form.get("<name>")
+    product_name = request.form.get("product-input")
+    product_price = request.form.get("product-price")
+    product_quantity = request.form.get("product-qantity")
+    # update stock
+    new_product = (
+        name = product_name,
+        price = product_price,
+        quantity = product_quantity,
+    )
+    # save stock
+    save_data(data)
+    return redirect("/purchaseform")
+return render_template("purchaseform.html")
 
 #http://127.0.0.1:5000/saleform
 @app.route("/saleform")
