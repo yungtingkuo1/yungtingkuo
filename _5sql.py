9707d54905b670e7f6ba4f830c30ec22db1b58f2
yungtingkuo1 <135653791+yungtingkuo1@users.noreply.github.com>
2023-09-20 21:03:00 +0100

    Add files via upload

--- _5sql.py
+++ _5sql.py
@@ -1,3 +1,4 @@
+from _3flask import load_data, save_data
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -9,11 +10,22 @@ app = Flask(__name__)
 app.config["SQLALCHEMY-DATABASE-URI"] = "sqlite:///products.sqlite3"
 db = SQLAlchemy(app)
 
+class Balance(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    balance = db.Column(db.Integer, nullable = False)
+
 class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120), unique = True, nullable = False)
     quantity = db.Column(db.Float, nullable = False)
-    balance = db.Column(db.Float, nullable = False)
-    history = db.Column(db.String(40))
+
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    entry = db.Column(db.String(255), nullable = False)
+
+inventory_data = load_data({})
+balance_data = load_data(0)
+history_data = load_data([])
 
 with app.app_context():
     db.create_all()
@@ -28,14 +40,10 @@ def add_product():
     if request.method == "POST":
         pn = request.form.get('product_name')
         pq = request.form.get('product_quantity')
-        pb = request.form.get('product_balance')
-        ph = request.form.get('pruduct_history')
 
         new_product = Product(
             name=pn,
             quantity=pq,
-            balance=pb,
-            history=ph
         )
 
         db.session.add(new_product)
@@ -50,9 +58,36 @@ def edit_product(idx):
     if request.method == "POST":
         product.name = request.form.get("product_name")
         product.quantity = request.form.get("product_quantity")
-        product.balance = float(request.form.get("product_balance"))
-        product.history = request.form.get("product_history")
+
         db.session.add(product)
         db.session.commit()
         return redirect(url_for("index"))
     return render_template("edit_product.html", product=product)
+
+
+@app.route("/balancechangeform", methods=["GET", "POST"])
+def balance():
+    if request.method == "POST":
+        balance_data = load_data(0)
+        history_data = load_data([])
+
+        amount = float(request.form.get("amount"))
+        if balance_data + amount < 0:
+            print("Not allowed to take a loan.")
+            return redirect(url_for("balance"))
+
+        balance_data += amount
+        history_data.append(f"Changing the account balance by: {amount}")
+
+        save_data(balance_data)
+        save_data(history_data)
+
+        return redirect(url_for("balance"))
+    return render_template("balancechangeform.html")
+
+
+@app.route("/history")
+@app.route("/history/<int:start>/<int:stop>/")
+def history(start=None, stop=None):
+    history_data = load_data([])
+    return render_template("history.html", history=history_data[start:stop])
